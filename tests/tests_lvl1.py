e29f99f592d9d7727e8fedeccc61c389a60e5b9a
test <test@test.com>
2026-06-03 15:08:58 +0100

    fix 2

--- tests/tests_lvl1.py
+++ tests/tests_lvl1.py
@@ -3,11 +3,12 @@ from demo.calc import Calculator
 
 class TestOperations(unittest.TestCase):
     def test_sum(self):
-        calculator = Calculator(8,2)
+        calculator = Calculator(8, 2)
         self.assertEqual(calculator.get_sum(), 10, "The answer was not 10.")
 
     def test_diff(self):
-        calculator = Calculator(10,20)
-        self.assertEqual(Calculator.get_diff(),10, "The answer was not 10.")
+        calculator = Calculator(10, 20)
+        self.assertEqual(calculator.get_diff(), -10, "The answer was not -10.")
+
 if __name__ == "__main__":
     unittest.main()
